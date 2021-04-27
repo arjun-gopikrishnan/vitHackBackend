14a744e69ddc61fcfe3f5fcb932d24d18586d856
Arjun <cosmicpepe2001@gmail.com>
2021-04-27 18:13:12 +0530

    CORS

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 const port = process.env.PORT;
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 
