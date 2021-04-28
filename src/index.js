44d7b2c9fba811325fbde44fab5ba6ba69ea7996
Arjun <cosmicpepe2001@gmail.com>
2021-04-28 10:10:21 +0530

    CORS bug #2

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const cors = require('cors')
+app.use(cors())
+app.options('*', cors())
+
 
 app.use(express.json());
 
